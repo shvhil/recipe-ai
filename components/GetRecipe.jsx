81ac18b9f0d0a678a5deb7559700916d7d5393d9
Twilight <shvhil@outlook.com>
2024-12-13 23:54:16 +0530

    added generate recipe button

---
+++ components/GetRecipe.jsx
@@ -0,0 +1,8 @@
+ export default function GetRecipe() {
+    return(
+	<div className="call-to-action-container">
+	    <p> Done adding ingredients? </p>
+	<button className="generate-btn btn" name="generate"> Generate Recipe </button> 
+	</div>
+    )
+ }
